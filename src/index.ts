67a79b8ac5cb2aad1c3bf05ae8e83e20500202ce
fabiankachlock <fabian.kachlock@icloud.com>
2022-05-05 17:31:19 +0200

    feat: improve index file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { isBrowser } from './lib/isBrowser';
+import { LogRushStream } from './stream';
 
 if (!isBrowser()) {
   await import('isomorphic-fetch');
 }
 
 export { LogRushClient } from './client';
+export { LogRushStream } from './stream';
+
+export async function createStream(name: string, id?: string, key?: string): Promise<LogRushStream> {
+  const stream = new LogRushStream(this.options, name, id, key);
+  await stream.register();
+  this.streams[stream.id] = stream;
+  return stream;
+}

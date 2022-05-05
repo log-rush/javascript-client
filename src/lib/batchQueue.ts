de1d4cb37a77e31ded231db76c9116fb20ee8bb8
fabiankachlock <fabian.kachlock@icloud.com>
2022-05-05 17:23:35 +0200

    feat: implement batch queue

--- src/lib/batchQueue.ts
+++ src/lib/batchQueue.ts
@@ -1,11 +1,23 @@
 export class BatchQueue<T> {
   private queue: T[];
 
-  constructor(public readonly maxSize: number, private readonly overflowHandler: (items: T[]) => Promise<void>) {}
+  constructor(public readonly maxSize: number, private readonly overflowHandler: (items: T[]) => Promise<void>) {
+    this.queue = [];
+  }
 
-  public push(item: T) {}
+  public push(item: T) {
+    this.queue.push(item);
+    if (this.queue.length >= this.maxSize) {
+      this.forceExecute();
+    }
+  }
 
-  public async forceExecute() {}
+  public async forceExecute() {
+    await this.overflowHandler([...this.queue]);
+    this.queue = [];
+  }
 
-  public clear() {}
+  public clear() {
+    this.queue = [];
+  }
 }

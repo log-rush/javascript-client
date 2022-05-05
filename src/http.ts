b08354d40130a2187c6e855fadf8f46d3b853edc
fabiankachlock <fabian.kachlock@icloud.com>
2022-05-05 17:09:17 +0200

    feat: implement log rush http api

--- src/http.ts
+++ src/http.ts
@@ -1,6 +1,89 @@
+import { LogRushStream } from './stream';
+import { LogRushApiErrorResponse, LogRushApiSuccessResponse, LogRushLog } from './types';
+
 export const LogRushHttpApi = {
-  registerStream(name: string, id?: string, key?: string) {},
-  unregisterStream(name: string, id?: string, key?: string) {},
-  log(name: string, id?: string, key?: string) {},
-  batch(name: string, id?: string, key?: string) {},
+  async registerStream(url: string, name: string, id?: string, key?: string): Promise<LogRushStream> {
+    const req = await fetch(`${url}stream/register`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        alias: name,
+        id: id ?? '',
+        key: key ?? '',
+      }),
+    });
+
+    if (req.status !== 200) {
+      throw new Error('cant register log stream: ' + (await req.json()));
+    }
+
+    return (await req.json()) as LogRushStream;
+  },
+  async unregisterStream(
+    url: string,
+    id?: string,
+    key?: string
+  ): Promise<LogRushApiErrorResponse | LogRushApiSuccessResponse> {
+    const req = await fetch(`${url}stream/unregister`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: id ?? '',
+        key: key ?? '',
+      }),
+    });
+    return await req.json();
+  },
+  async log(
+    url: string,
+    logStream: string,
+    log: LogRushLog
+  ): Promise<LogRushApiErrorResponse | LogRushApiSuccessResponse> {
+    const req = await fetch(`${url}batch`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        stream: logStream,
+        log: log.log,
+        timestamp: log.timestamp,
+      }),
+    });
+
+    if (req.status !== 200) {
+      throw new Error('cant log to stream ' + logStream + ' : ' + (await req.json()));
+    }
+
+    return (await req.json()) as LogRushApiErrorResponse | LogRushApiSuccessResponse;
+  },
+  async batch(
+    url: string,
+    logStream: string,
+    logs: LogRushLog[]
+  ): Promise<LogRushApiErrorResponse | LogRushApiSuccessResponse> {
+    const req = await fetch(`${url}batch`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        stream: logStream,
+        logs: logs.map(log => ({
+          log: log.log,
+          timestamp: log.timestamp,
+        })),
+      }),
+    });
+
+    if (req.status !== 200) {
+      throw new Error('cant log to stream ' + logStream + ' : ' + (await req.json()));
+    }
+
+    return (await req.json()) as LogRushApiErrorResponse | LogRushApiSuccessResponse;
+  },
 };
